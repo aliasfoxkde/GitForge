bd5e6a707e0f9b5c56e1d578ecccd9447f362f35
Micheal Kinney <micheal.l.c.kinney@gmail.com>
2026-09-22 10:37:07 -0500

    style(ci): remove duplicate scheduler import

--- services/ci/src/main.rs
+++ services/ci/src/main.rs
@@ -22,10 +22,9 @@ use gitforge_events::{
     PushReceivedPayload,
 };
 use gitforge_process::{create_shutdown_flag, spawn_shutdown_handler, wait_for_shutdown};
-use gitforge_scheduler::assigner::JobExecutionDefinition;
 use gitforge_scheduler::{
-    assigner::DEFAULT_JOB_TIMEOUT_SECS, create_state_with_artifact_storage, scheduler_routes,
-    Scheduler, SchedulerEvent,
+    assigner::{JobExecutionDefinition, DEFAULT_JOB_TIMEOUT_SECS},
+    create_state_with_artifact_storage, scheduler_routes, Scheduler, SchedulerEvent,
 };
 use gitforge_storage::FileStorage;
 use std::collections::{HashMap, HashSet};
